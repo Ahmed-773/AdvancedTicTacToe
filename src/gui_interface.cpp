c7d9824e96f0b9db250aa52e3e8868a6f04eec93
Ahmed-773 <a1hmedabdelfattah@gmail.com>
2025-06-19 14:20:36 +0300

    THE LAST DANCE edited2

--- src/gui_interface.cpp
+++ src/gui_interface.cpp
@@ -530,9 +530,15 @@ void GUIInterface::loadUserGames() {
 }
 
 void GUIInterface::displayGame(const GameState& game) {
-    gameLogic = gameHistory.replayGame(game.gameId);
+    gameLogic.resetBoard();
+.
+    for (const Move& move : game.moveHistory) {
+        gameLogic.makeMove(move.row, move.col);
+    }
+
     switchToGameView();
-    updateBoard();
+    updateBoard(); 
+
     statusLabel->setText("Game Replay: " + QString::fromStdString(game.timestamp));
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
