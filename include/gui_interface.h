4b1d0f3a904b4b733bcb46161adf9cddc9bd870d
Ahmed-773 <a1hmedabdelfattah@gmail.com>
2025-06-22 16:34:27 +0300

    THE LAST DANCE

--- include/gui_interface.h
+++ include/gui_interface.h
@@ -1,4 +1,4 @@
-// gui_interface.h - Optimized Version
+// File: gui_interface.h - Final Version (Sound Removed)
 #ifndef GUI_INTERFACE_H
 #define GUI_INTERFACE_H
 
@@ -34,22 +34,9 @@
 #include <QDir>
 #include <string>
 
-// Forward declarations
-class QStackedWidget;
-class QLineEdit;
-class QLabel;
-class QPushButton;
-class QTableWidget;
-class QComboBox;
-class QRadioButton;
-class QTimer;
-class QPropertyAnimation;
-
-// Include all necessary headers for our own classes
 #include "user_auth.h"
 #include "game_logic.h"
 #include "ai_engine.h"
-#include "database_manager.h"
 #include "game_history.h"
 
 class GUIInterface : public QMainWindow {
@@ -60,25 +47,17 @@ public:
     ~GUIInterface();
 
 private slots:
-    // Authentication
     void onLoginButtonClicked();
     void onRegisterButtonClicked();
     void onLogoutButtonClicked();
     void onGuestPlayClicked();
-
-    // Game control
     void onCellClicked();
     void onNewGameButtonClicked();
     void onUndoMoveClicked();
     void onHintClicked();
-
-    // Game modes and settings
     void onGameModeChanged();
-    void onThemeChanged();
-    void onSoundToggled();
-    void onAnimationSpeedChanged();
-
-    // Game history and Replay
+    void onThemeChanged(int id);
+    void onAnimationSpeedChanged(int value);
     void onViewHistoryClicked();
     void onViewStatsClicked();
     void onGameHistoryItemClicked(int row, int column);
@@ -87,10 +66,7 @@ private slots:
     void onReplayPrevClicked();
     void onReplayStartClicked();
     void onReplayAutoPlay();
-
-    // Timers and animations
     void onGameTimerUpdate();
-    void onCellAnimationFinished();
 
 private:
     // Core backend components
@@ -101,9 +77,8 @@ private:
     DatabaseManager dbManager;
 
     // Themes and settings
-    enum Theme { DARK, LIGHT, NEON, CLASSIC };
+    enum Theme { DARK, LIGHT, NEON };
     Theme currentTheme;
-    bool soundEnabled;
     bool animationsEnabled;
     int animationSpeed;
 
@@ -114,130 +89,47 @@ private:
     QTimer* gameTimer;
     QTimer* aiThinkTimer;
 
-    // --- UI Structure ---
+    // UI Structure
     QStackedWidget *mainStack;
-
-    // --- Enhanced Login Screen ---
-    QWidget *loginWidget;
-    QFrame *loginFrame;
-    QLineEdit *usernameInput;
-    QLineEdit *passwordInput;
-    QPushButton *loginButton;
-    QPushButton *registerButton;
-    QPushButton *guestButton;
-    QLabel *loginStatusLabel;
-    QProgressBar *loginProgressBar;
-
-    // --- Enhanced Game Screen ---
-    QWidget *gameWidget;
-    QFrame *gameFrame;
-    QFrame *boardFrame;
+    QWidget *loginWidget, *gameWidget, *historyWidget, *statsWidget, *settingsWidget;
+    QFrame *navigationFrame, *loginFrame, *gameFrame, *boardFrame, *scoreFrame, *controlsFrame, *historyDetailsFrame, *statsFrame;
+    QLineEdit *usernameInput, *passwordInput;
     QPushButton *boardButtons[3][3];
-    QLabel *statusLabel;
-    QLabel *timerLabel;
-    QProgressBar *aiThinkingBar;
-    
-    // Game mode controls
-    QTabWidget *gameModeTab;
-    QWidget *pvpModeWidget;
-    QWidget *aiModeWidget;
-    QWidget *challengeModeWidget;
+    QLabel *statusLabel, *timerLabel, *loginStatusLabel, *playerXScoreLabel, *playerOScoreLabel, *streakLabel, *winRateLabel, *totalGamesLabel, *winRateStatsLabel, *averageGameTimeLabel, *longestStreakLabel, *favoriteOpponentLabel, *replayPositionLabel;
+    QProgressBar *aiThinkingBar, *loginProgressBar;
+    QTabWidget *gameModeTab, *settingsTab;
     QComboBox *difficultyCombo;
-    QSlider *aiSpeedSlider;
-    
-    // Score and stats
-    QFrame *scoreFrame;
-    QLabel *playerXScoreLabel;
-    QLabel *playerOScoreLabel;
-    QLabel *streakLabel;
-    QLabel *winRateLabel;
-    
-    // Game controls
-    QFrame *controlsFrame;
-    QPushButton *newGameButton;
-    QPushButton *undoButton;
-    QPushButton *hintButton;
-    QPushButton *pauseButton;
-
-    // --- Enhanced History Screen ---
-    QWidget *historyWidget;
-    QSplitter *historySplitter;
+    QSlider *aiSpeedSlider, *animationSpeedSlider, *replaySpeedSlider;
+    QPushButton *loginButton, *registerButton, *guestButton, *newGameButton, *undoButton, *hintButton, *pauseButton, *backToGameButton, *exportHistoryButton;
     QTableWidget *gameHistoryTable;
-    QFrame *historyDetailsFrame;
+    QSplitter *historySplitter;
     QTextEdit *gameDetailsText;
-    QPushButton *backToGameButton;
-    QPushButton *exportHistoryButton;
-
-    // --- Settings Screen ---
-    QWidget *settingsWidget;
-    QTabWidget *settingsTab;
-    QWidget *appearanceTab;
-    QWidget *gameplayTab;
-    QWidget *soundTab;
     QButtonGroup *themeGroup;
-    QCheckBox *soundEnabledCheck;
     QCheckBox *animationsEnabledCheck;
-    QSlider *animationSpeedSlider;
     QSpinBox *autoSaveSpinBox;
-
-    // --- Statistics Screen ---
-    QWidget *statsWidget;
-    QScrollArea *statsScrollArea;
-    QFrame *statsFrame;
-    QLabel *totalGamesLabel;
-    QLabel *winRateStatsLabel;
-    QLabel *averageGameTimeLabel;
-    QLabel *longestStreakLabel;
-    QLabel *favoriteOpponentLabel;
-
-    // --- Replay Controls ---
     QFrame *replayControlsFrame;
-    QPushButton *replayStartButton;
-    QPushButton *replayPrevButton;
-    QPushButton *replayNextButton;
-    QPushButton *replayAutoButton;
-    QSlider *replaySpeedSlider;
-    QLabel *replayPositionLabel;
+    QPushButton *replayStartButton, *replayPrevButton, *replayNextButton, *replayAutoButton;
+    QPushButton *gameNavButton, *historyNavButton, *statsNavButton, *settingsNavButton;
     QTimer *replayAutoTimer;
     std::vector<Move> replayHistory;
     int replayMoveIndex;
     bool replayAutoMode;
 
-    // --- Navigation ---
-    QFrame *navigationFrame;
-    QPushButton *gameNavButton;
-    QPushButton *historyNavButton;
-    QPushButton *statsNavButton;
-    QPushButton *settingsNavButton;
-
-    // --- Animations ---
-    QPropertyAnimation *boardAnimation;
-    QParallelAnimationGroup *cellAnimations;
-    QGraphicsOpacityEffect *fadeEffect;
-
-    // --- Private Helper Methods ---
+    // Private Helper Methods
     void setupUI();
+    void setupNavigation();
     void setupAuthentication();
     void setupGameBoard();
     void setupHistoryView();
     void setupStatsView();
     void setupSettingsView();
-    void setupNavigation();
     void setupReplayControls();
-    void setupAnimations();
     
-    // Styling and themes
     void applyTheme(Theme theme);
-    void applyDarkTheme();
-    void applyLightTheme();
-    void applyNeonTheme();
-    void applyClassicTheme();
     void updateButtonStyles();
-    void animateButton(QPushButton* button);
+    void animateButton(QWidget* widget);
     void addDropShadow(QWidget* widget);
-    void addGlowEffect(QWidget* widget, const QColor& color);
     
-    // Game logic helpers
     void updateBoard(bool isReplay = false);
     void updateScoreDisplay();
     void updateGameStats();
@@ -248,7 +140,6 @@ private:
     void highlightWinningCells(const std::vector<Move>& cells);
     void resetBoardHighlights();
     
-    // Navigation helpers
     void switchToLoginView();
     void switchToGameView();
     void switchToHistoryView();
@@ -256,32 +147,23 @@ private:
     void switchToSettingsView();
     void updateNavigationButtons();
     
-    // History and replay helpers
     void loadUserGames();
     void displayGameForReplay(const GameState& game);
     void updateReplayControls();
     void exportGameHistory();
     
-    // Settings helpers
     void loadSettings();
     void saveSettings();
     void applySettings();
     
-    // Animation helpers
     void animateCellPlacement(int row, int col, Player player);
-    void animateGameOver(GameResult result);
-    void animateScoreUpdate();
     void fadeInWidget(QWidget* widget);
-    void fadeOutWidget(QWidget* widget);
-    void slideInWidget(QWidget* widget);
     
-    // Utility methods
     QString formatTime(int seconds);
     QString formatGameResult(GameResult result);
     QString getPlayerName(Player player);
     QColor getPlayerColor(Player player);
     void showNotification(const QString& message, const QString& type = "info");
-    void playSound(const QString& soundName);
 };
 
 #endif // GUI_INTERFACE_H
